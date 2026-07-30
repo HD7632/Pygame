ea2e34374769df7c9e6fd21a4ffa7973d663c366
HD7632 <nijimeneza@unal.edu.co>
2026-07-30 01:22:37 -0500

    Update bottles.py
    Updated the Bottles class to use initial velocities for both x and y directions. Fixed the hitbox position update to reflect the bottle's current position.

--- UnaFreud/bottles.py
+++ UnaFreud/bottles.py
@@ -3,10 +3,10 @@ from physics import Physics
 
 physics = Physics()
 class Bottles:
-    def __init__(self, x, y, v0x, x0y):
+    def __init__(self, x, y, v0x, v0y):
         self.pos = [x, y]
-        self.vx = 1
-        self.vy = 0
+        self.vx = v0x
+        self.vy = v0y
         self.hitbox = pygame.Rect(x, y, 5, 5)
 
     def grvty(self, dt):
@@ -16,7 +16,10 @@ class Bottles:
         pygame.draw.rect(screen, (0, 0, 255), self.hitbox)
 
     def update_bottles(self, dt):
+
         self.grvty(dt)
         self.pos[0] = self.pos[0]+self.vx*dt
         self.pos[1] = self.pos[1]+self.vy*dt
+        self.hitbox.midbottom = (round(self.pos[0]), round(self.pos[1]))
+
         self.drawbottle
